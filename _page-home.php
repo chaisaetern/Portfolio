e280966a32ff3f74e74aa831f4fbb5f57c42693b
Chai <csaetern@alphaworks.tech>
2022-06-10 18:04:43 -0700

    home page commit

--- _page-home.php
+++ _page-home.php
@@ -8,8 +8,23 @@
 ?>
 <?php get_header(); ?>
 
-<div class="home-container">
-    
+<div class="main-container">
+    <div class="main-wrapper">
+
+        <div class="banner-container">
+            <h2 class="banner-title">
+                <?php the_field("banner_title"); ?>
+            </h2>
+            <div class="banner-wrapper">
+                <!-- <?php get_template_part('template-parts/projects-showcase') ?> -->
+            </div>
+        </div>
+
+        <div class="bg-container">
+            <img class="bg-img" src="<?php the_field("background_image_1"); ?>" alt="">
+        </div>
+    </div>
+
 </div>
 
 <?php get_footer(); ?>
---
+++ template-parts/projects-showcase.php
@@ -0,0 +1,29 @@
+<div class="projects-container">
+    <h2 class="projects-title">My Projects</h2>
+    <div class="projects-wrapper">
+        <div class="project">
+            <span></span>
+            <div class="project-content">
+                <h2>Project One</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis perferendis nobis nemo odio unde nostrum fugit vero, eligendi qui dolorum debitis. Fugiat nesciunt porro dolore labore omnis optio facilis soluta.</p>
+                <a href="#">Read More</a>
+            </div>
+        </div>
+        <div class="project">
+            <span></span>
+            <div class="project-content">
+                <h2>Project Two</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis perferendis nobis nemo odio unde nostrum fugit vero, eligendi qui dolorum debitis. Fugiat nesciunt porro dolore labore omnis optio facilis soluta.</p>
+                <a href="#">Read More</a>
+            </div>
+        </div>
+        <div class="project">
+            <span></span>
+            <div class="project-content">
+                <h2>Project Three</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis perferendis nobis nemo odio unde nostrum fugit vero, eligendi qui dolorum debitis. Fugiat nesciunt porro dolore labore omnis optio facilis soluta.</p>
+                <a href="#">Read More</a>
+            </div>
+        </div>
+    </div>
+</div>

d02f6120fb138d5b026d787fbd68183abdfbb870
Chai <csaetern@alphaworks.tech>
2022-06-14 14:46:27 -0700

    about page commit

--- _page-about.php
+++ _page-about.php
@@ -9,11 +9,21 @@
 <?php get_header(); ?>
 
     <div class="about-container">
-        <div class="intro">
+
+        <div class="about-bg-container">
+            <div class="loading-screen-container">
+                <!-- <img src="<?php the_field("loading_screen"); ?>" alt="" class="loading-screen"> -->
+                <!-- <img src="<?php the_field("loading"); ?>" alt="" class="loading"> -->
+    
+            </div>
+        </div>
+
+        <!-- <div class="intro">
             <h2>Hello.</h2>
             <h2>Welcome!</h2>
             <h2>My name is Chai.</h2>
-        </div>
+        </div> -->
+
     </div>
 
 <?php get_footer(); ?>

385d53d9daedcf28c7f8724b597f20bb9cb94fa2
Chai <csaetern@alphaworks.tech>
2022-06-16 19:19:01 -0700

    about page commit

--- _page-about.php
+++ _page-about.php
@@ -12,7 +12,16 @@
     <?php get_template_part('template-parts/socials') ?>
     
     <div class="about-container">
+
         <img src="<?php the_field('about_bg'); ?>" alt="" class="about-bg">
+
+        <div class="about-content">
+            <h2>About Header</h2>
+            <div class="about-info">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor facilis qui reiciendis placeat cupiditate, illo provident nam maxime odit quaerat ex consequuntur error. Illo omnis culpa, voluptatibus dolore et repellendus!</p>
+            </div>
+        </div>
+
     </div>
 
 <?php get_footer(); ?>

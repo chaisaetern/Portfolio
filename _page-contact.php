bf002886f08601f84491f09e49701a2917890461
Chai <csaetern@alphaworks.tech>
2022-06-16 20:10:46 -0700

    flexbox library disabled

--- _page-contact.php
+++ _page-contact.php
@@ -13,6 +13,10 @@
         <?php get_template_part('template-parts/loading') ?>
         <?php get_template_part('template-parts/socials') ?>
         
+        <div class="coming-soon">
+            <h2 class="coming">COMING</h2>
+            <h2 class="coming">SOON</h2>
+        </div>
     </div>
     
 <?php get_footer(); ?>

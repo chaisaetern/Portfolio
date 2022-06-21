983925815763bde1c2d6aad1e78b58d5c250c444
Chai <csaetern@alphaworks.tech>
2022-06-21 12:12:05 -0700

    page about commit

--- _page-about.php
+++ _page-about.php
@@ -16,9 +16,14 @@
         <img src="<?php the_field('about_bg'); ?>" alt="" class="about-bg">
 
         <div class="about-content">
-            <h2>About Header</h2>
             <div class="about-info">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor facilis qui reiciendis placeat cupiditate, illo provident nam maxime odit quaerat ex consequuntur error. Illo omnis culpa, voluptatibus dolore et repellendus!</p>
+                <div class="about-img-portfolio">
+                    <img src="<?php the_field('about_img'); ?>" alt="" class="about-img">
+                </div>
+                <div class="about-info-content">
+                    <h2 class="about-title">About Me</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor facilis qui reiciendis placeat cupiditate, illo provident nam maxime odit quaerat ex consequuntur error. Illo omnis culpa, voluptatibus dolore et repellendus!</p>
+                </div>
             </div>
         </div>
 

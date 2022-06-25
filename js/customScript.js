6a9d64934b885bc2bffdbea8e28dda8975880976
Chai <csaetern@alphaworks.tech>
2022-06-25 04:06:21 -0700

    update commit

--- js/customScript.js
+++ js/customScript.js
@@ -5,40 +5,43 @@ const $skill = $('.skill');
 const $skillTitle = $('.skill-title');
 const $bar = $('.bar');
 
+
+
 // Map through each bar to trigger
 // skill bar fill animation
-$bar.map(($i, $e) => {
+$bar.map($i => {
 
     // Store each child element and its value
     let $skillBar = $(`.skill:nth-child(${$i+1}) .bar`);
     let $skillEffc = $(`.skill:nth-child(${$i+1}) .skill-effc`);
-    let $skillValue = $skillEffc.attr('value');
+    let $skillValue = parseInt($skillEffc.attr('value'));
+    let $defValue = 0;
+    
+        // Num Percent animation
+        let counter = () => setInterval(() => {
 
-    // When hovering over each child element
-    $(`.skill:nth-child(${$i+1})`).hover(() => {
+            $defValue++;
+            
+            if ($defValue >= $skillValue) {
+                $defValue = $skillValue;
+                clearInterval(counter);
+            }
+            
+            $skillEffc.text(`${$defValue}%`);
+
+        }, 30)
+
+        setTimeout(() => {
+            counter();
+            $skillBar.animate({
+                'width': `${$skillValue}%`,
+            }, 1800);
+        }, 5000);
         
         // Animate the width based on skill value
         // provided in the HTML element attribute value
-        $skillBar.animate({
-            'width': $skillValue
-        }, 555);
 
         // Show the text value
-        $skillEffc.text($skillValue);
-
-    },
-
-    // When not hovering reset the values to its defaults
-        () => {
-            $skillBar.animate({
-                'width': '0%'
-            }, 555);
-
-            // Reset text value
-            $skillEffc.text('');
-        }
-    );
+        $skillEffc.text(`${$defValue}%`);
 
 });
-
-
--- template-parts/skills.php
+++ template-parts/skills.php
@@ -9,15 +9,17 @@
 
 
 <div class="skills-container">
+    <h3 class="skill-header">Skills &amp;</h3>
+    <h3 class="skill-header">Proficiency</h3>
     <div class="skills-wrapper">
 
     <?php
         $skillTypes = array(
-            array('lang' => 'HTML', 'effc' => '85%'),
-            array('lang' => 'CSS / SCSS', 'effc' => '80%'),
-            array('lang' => 'JavaScript / ES6+', 'effc' => '55%'),
-            array('lang' => 'PHP / WordPress', 'effc' => '33%'),
-            array('lang' => 'Engrish', 'effc' => '50%'),
+            array('lang' => 'HTML5', 'effc' => '68', 'icon' => 'fa-brands fa-html5'),
+            array('lang' => 'CSS3 | SCSS', 'effc' => '65', 'icon' => 'fa-brands fa-css3'),
+            array('lang' => 'JavaScript | ES6+ | jQuery', 'effc' => '57', 'icon' => 'fa-brands fa-js-square'),
+            array('lang' => 'PHP | WordPress', 'effc' => '42', 'icon' => 'fa-brands fa-wordpress'),
+            array('lang' => 'Engrish ', 'effc' => '-99', 'icon' => 'fa-solid fa-poop'),
         );
 
         for($s = 0; $s < count($skillTypes); ++$s) {
@@ -25,8 +27,11 @@
         
         <div class="skill">
             <h3 class="skill-title">
-                <?php echo $skillTypes[$s]['lang']; ?>
-                <span value="<?php echo $skillTypes[$s]['effc']; ?>" class="skill-effc"></span>
+                    <div class="skill-icon">
+                        <div class="skill-icon <?php echo $skillTypes[$s]['icon']; ?>"></div>
+                        <?php echo $skillTypes[$s]['lang']; ?>
+                    </div>
+                <span value="<?php echo $skillTypes[$s]['effc']; ?>" class="skill-effc">0%</span>
             </h3>
             <div class="skill-bar">
                 <div class="bar"></div>

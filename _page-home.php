a421d5655007c3edaacf8879c8a6ef08ac561049
Chai <csaetern@alphaworks.tech>
2022-07-05 14:34:23 -0700

    init commit

--- _page-home.php
+++ _page-home.php
@@ -14,28 +14,26 @@
     <?php get_template_part('template-parts/loading') ?>
 
     <div class="main-wrapper">
+
         <div class="background-image-1-container">
             <img src="<?php the_field('background_image_1') ?>" alt="" class="background-image-1">
         </div>
+
         <div class="hero-container">
+            
             <div class="hero-wrapper">
-                <div class="hero"></div>
-                <div class="hero"></div>
-                <div class="hero"></div>
-                <div class="hero"></div>
-                <div class="hero"></div>
-                <div class="hero"></div>
-                <div class="hero"></div>
-                <!-- <div class="hero-text-container">
-                    <h3 class="hero-title">Hi, and welcome!</h3>
-                </div> -->
+                
             </div>
+
             <div class="arrow-down-container">
                 <i class="fa-solid fa-arrow-down"></i>
             </div>
-            <?php get_template_part('template-parts/particles') ?>
+            
         </div>
+        
     </div>
+
+    <?php get_template_part('template-parts/particles') ?>
 </div>
 
 <?php get_footer(); ?>
